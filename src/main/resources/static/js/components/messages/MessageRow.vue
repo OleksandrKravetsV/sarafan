4424c77690b883467af1c87ed73bd09347c54d32
Oleksandr Kravets <kravetsalexandrv@gmail.com>
2022-07-01 23:47:52 +0300

    add Open Graph Protocol, link preview

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -1,33 +1,36 @@
 <template>
-  <v-card class="my-2">
-        <v-card-title primary-text>
-            <i>({{ message.id }})</i>
-            {{ message.text }}
-        </v-card-title>
-        <v-card-actions >
-            <v-btn value="Edit" @click="edit" small flat round>Edit</v-btn>
-            <v-btn icon @click="del" small>
-              <v-icon>delete</v-icon>
-            </v-btn>
+    <v-card class="my-2">
+          <v-card-text primary-title>
+              <i>({{ message.id }})</i>
+              {{ message.text }}
+          </v-card-text>
+          <media v-if="message.link" :message="message"></media>
+          <v-card-actions>
+              <v-btn value="Edit" @click="edit" small flat round>Edit</v-btn>
+              <v-btn icon @click="del" small>
+                  <v-icon>delete</v-icon>
+              </v-btn>
         </v-card-actions>
     </v-card>
 </template>
 
 <script>
-    import {mapActions} from 'vuex'
+  import { mapActions } from 'vuex'
+  import Media from 'components/media/Media.vue'
 
-    export default {
-        props: ['message', 'editMessage'],
-        methods: {
-            ...mapActions(['removeMessageAction']),
-            edit() {
-               this.editMessage(this.message)
-            },
-            del() {
-                this.removeMessageAction(this.message)
-            }
-        }
-    }
+  export default {
+      props: ['message', 'editMessage'],
+      components: { Media },
+      methods: {
+          ...mapActions(['removeMessageAction']),
+          edit() {
+            this.editMessage(this.message)
+          },
+          del() {
+            this.removeMessageAction(this.message)
+          }
+      }
+  }
 </script>
 
 <style>
