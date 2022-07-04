9134552bde73b89ffc1165fa3b31d40aee559e2b
Oleksandr Kravets <kravetsalexandrv@gmail.com>
2022-07-04 23:34:22 +0300

    Solution of circular references in JSON. vue debug.

--- src/main/resources/static/js/components/comment/CommentItem.vue
+++ src/main/resources/static/js/components/comment/CommentItem.vue
@@ -1,15 +1,20 @@
 <template>
-    <v-list-tile>
-        <v-list-tile-content>
-           <v-list-tile-title>{{comment.text}}</v-list-tile-title>
-        </v-list-tile-content>
-    </v-list-tile>
+  <v-list-tile>
+    <v-list-tile-avatar>
+      <img  :src="comment.author.userpic" referrerpolicy="no-referrer" >
+    </v-list-tile-avatar>
+
+    <v-list-tile-content>
+      <v-list-tile-title>{{comment.text}}</v-list-tile-title>
+      <v-list-tile-sub-title>{{comment.author.name}}</v-list-tile-sub-title>
+    </v-list-tile-content>
+  </v-list-tile>
 </template>
 
 <script>
 export default {
-    name: 'CommentItem',
-    props: ['comment']
+  name: 'CommentItem',
+  props: ['comment']
 }
 </script>
 
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import '@babel/polyfill'
 import 'api/resource'
-import router from "router/router"
+import router from 'router/router'
 import App from 'pages/App.vue'
-import store from "store/store"
+import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
 
